abfffcdb57714e82353c2647e6c114d8834fc2b8
Hong Jeon <hongj77@gmail.com>
2014-06-03 17:27:11 -0500

    fixed search function

--- maincontroller.js
+++ maincontroller.js
@@ -1,72 +1,17 @@
 app.controller("MainController", function($scope) {
-
-	$scope.selectedElement = "All";
 	$scope.searchString = "";
-	$scope.things = [
-		"All",
-		"Name", 
-		"Message",
-		"Action"
+	$scope.messages = [ {"entryKey": "BBBBBB"}, 
+						{
+							"entryKey":"AAAAAAAAAFQfiwgAAAAAAAADY2AAA4dqpcTSlMySkMqCVCUrJfei_NICR5CAkg5EwjMFKJxvamyRamJsBoSGiWmGBsbGZgZgoFRbXl6ul5iYXAwAAZmu1FAAAAA=",
+							"eventEntry":[
+											{
+												"tag":"EventStateChange",
+												"contents":{
+																"stateChangeMsg":"{\"auditUser\":\"hongj77@gmail.com\",\"auditAction\":\"Create\",\"auditMessage\":{\"groupName\":\"Test\"}}",
+																"stateChangeTime":1401832292
+															}
+											}
+										]
+						}
 	];
-
-	$scope.messages = [
-		{
-			name: "Jim Halpert", 
-			action: "GET", 
-			message: "adsfasdfasdfasdfasdfasdfasdf"
-		},
-
-		{
-			name: "Kevin Durant", 
-			action: "DELETE", 
-			message: "asdfasdfaasasdsdfsadasdsaddsasfsadfsadfs"
-		},
-
-		{
-			name: "Ibaka", 
-			action: "DELETE", 
-			message: "asdfasdfaasasdsdfsadasdsaddsasfsadfsadfs"
-		},
-
-		{
-			name: "Russell westBrook", 
-			action: "POST", 
-			message: "asdfasdfaasasdsdfsadasdsaddsasfsadfsadfs"
-		},
-
-		{
-			name: "steven adams", 
-			action: "DELETE", 
-			message: "asdfasdfaasasdsdfsadasdsaddsasfsadfsadfs"
-		},
-
-		{
-			name: "Derick Fisher's biceps", 
-			action: "GET", 
-			message: "asdfasdfaasasdsdfsadasdsaddsasfsadfsadfs"
-		}
-	];
-
-	//search filter
-	app.filter('searchFor', function() {
-		return function (array, searchString) {
-
-			if (!searchString || arr.length <= 1)
-				return arr;
-
-			searchString = searchString.toLowerCase();
-			var resultArr = []
-			var whereSearch = selectedElement;
-
-			for (i = 0; i < arr.length; i++) {
-				var currentValue = arr[i].whereSearch.toLowerCase;
-
-				if (currentValue.indexOf(searchString) !== -1)
-					resultArr.push(arr[i]);
-			}
-
-			return resultArr;
-		};
-	});
-
 });
