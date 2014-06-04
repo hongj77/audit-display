7ff3c8e11ddf4290a61473ff156323e99a665235
Hong Jeon <hongj77@gmail.com>
2014-06-04 13:29:27 -0500

    tables now accept multiple messages

--- maincontroller.js
+++ maincontroller.js
@@ -1,8 +1,9 @@
 app.controller("MainController", function($scope) {
 	$scope.searchString = "";
-	$scope.messages = [ {"entryKey": "BBBBBB"}, 
+	$scope.parsedMessages = [];
+	$scope.messages = [
 						{
-							"entryKey":"AAAAAAAAAFQfiwgAAAAAAAADY2AAA4dqpcTSlMySkMqCVCUrJfei_NICR5CAkg5EwjMFKJxvamyRamJsBoSGiWmGBsbGZgZgoFRbXl6ul5iYXAwAAZmu1FAAAAA=",
+							"entryKey":"AAAAAAAAA",
 							"eventEntry":[
 											{
 												"tag":"EventStateChange",
@@ -12,6 +13,66 @@ app.controller("MainController", function($scope) {
 															}
 											}
 										]
+						}, 
+
+						{
+							"entryKey":"BccccBBBBBB",
+							"eventEntry":[
+											{
+												"tag":"EventStateChange",
+												"contents":{
+																"stateChangeMsg":"{\"auditUser\":\"hacker1234@gmail.com\",\"auditAction\":\"Delete\",\"auditMessage\":{\"groupName\":\"Admin\"}}",
+																"stateChangeTime":1123132292
+															}
+											}
+										]
+						},
+
+						{
+							"entryKey":"BccccBasdasdBBBBB",
+							"eventEntry":[
+											{
+												"tag":"EventStateChange",
+												"contents":{
+																"stateChangeMsg":"{\"auditUser\":\"hacker1234@gmail.com\",\"auditAction\":\"Delete\",\"auditMessage\":{\"groupName\":\"Admin\"}}",
+																"stateChangeTime":1123132292
+															}
+											}
+										]
 						}
 	];
+
+	_.each($scope.messages, function (msg) {
+
+		var parsed = JSON.parse(msg.eventEntry[0].contents.stateChangeMsg);
+
+		var makeMessageArray = function (stringMsg) {
+			var result = [];
+			var inside = {};
+			inside.title = "Entry Key";
+			inside.value = msg.entryKey;
+			result.push(inside);
+
+			_.each(stringMsg, function (value, key) {
+				if (!(value instanceof Object)) {
+					var inside = {};
+					inside.title = key;
+					inside.value = value;
+					result.push(inside);
+				}
+				else {
+						_.each(value, function (value, key) {
+						var inside = {};
+						inside.title = key;
+						inside.value = value; 
+						result.push(inside);
+					});
+				}
+			});
+			return result;
+		};
+
+		$scope.parsedMessages.push(makeMessageArray(parsed));
+	});
+
 });
